03270588684d6267fcfa4722bfbe4d9e212f62dd
subhaishere <60627114+subhaishere@users.noreply.github.com>
2023-08-16 17:10:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+function removeSelectedColor() {
+    var colorSelect = document.getElementById("colorSelect");
+    var selectedIndex = colorSelect.selectedIndex;
+
+    if (selectedIndex !== -1) { // Check if an option is selected
+        colorSelect.remove(selectedIndex);
+    }
+}
